cddb122b1f65c2428ebb8a7f0bce3544acfc10d4
rodlumon35 <rodlumon35@gmail.com>
2022-04-19 22:33:13 +0100

    routes changed from services

--- src/services/project.ts
+++ src/services/project.ts
@@ -1,9 +1,1 @@
-import express from "express";
 
-const projectRouter = express.Router();
-
-projectRouter.get("/", (_, res) => {
-  res.send("getting all projects");
-});
-
-export default projectRouter;
